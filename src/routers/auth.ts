f85aaee37c244d0634addcb1b7c79cc6c8f6fcb9
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-07-27 15:58:10 +0200

    Refactor register route

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authValidation } from "../config/authValidation";
+import { loginUser } from "../controllers/auth/loginUser";
 import { registerUser } from "../controllers/auth/register";
 import ROUTES from "./routes";
 
 const AuthRouter = Router();
 
-AuthRouter.post(ROUTES.AUTH.REGISTER, authValidation, registerUser);
+AuthRouter.post(ROUTES.AUTH.REGISTER, authValidation(), registerUser);
+AuthRouter.post(ROUTES.AUTH.LOGIN, authValidation(), loginUser);
 
 export default AuthRouter;
