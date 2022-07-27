f32e237b107ac0dd0db8411250455153997ecc9d
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-07-27 17:08:18 +0200

    Add logout functionality

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authValidation } from "../config/authValidation";
 import { loginUser } from "../controllers/auth/loginUser";
+import { logoutUser } from "../controllers/auth/logoutUser";
 import { registerUser } from "../controllers/auth/register";
 import ROUTES from "./routes";
 
@@ -8,5 +9,6 @@ const AuthRouter = Router();
 
 AuthRouter.post(ROUTES.AUTH.REGISTER, authValidation(), registerUser);
 AuthRouter.post(ROUTES.AUTH.LOGIN, authValidation(), loginUser);
+AuthRouter.delete(ROUTES.AUTH.LOGOUT, logoutUser);
 
 export default AuthRouter;
