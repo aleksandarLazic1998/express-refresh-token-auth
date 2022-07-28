b2c66b53f7984b32c0ee9d5832030e1211d8203c
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-07-28 19:09:58 +0200

    Add admin priviledge for delete-ing employee

---
+++ src/controllers/employees/deleteEmployee.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { isValidObjectId } from "mongoose";
+import EmployeeModel from "../../models/employees";
+
+export const deleteEmployee = async (req: Request, res: Response) => {
+	const employeeId = req.params.id;
+
+	const isIdValid = isValidObjectId(employeeId);
+
+	if (!isIdValid) {
+		return res
+			.status(422)
+			.json({ errors: [{ msg: `Employee id: ${employeeId} is not valid.` }] });
+	}
+
+	const foundEmployee = await EmployeeModel.findOne({ _id: employeeId }).exec();
+
+	if (!foundEmployee) {
+		return res.status(404).json({
+			errors: [
+				{ msg: `There is no matching employee with id: ${employeeId}.` },
+			],
+		});
+	}
+
+	await EmployeeModel.deleteOne({ _id: employeeId });
+
+	return res.status(204);
+};
--- src/routers/employee.ts
+++ src/routers/employee.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { USER_ROLES } from "../constants/userRoles";
 import { addEmployee } from "../controllers/employees/addEmployee";
+import { deleteEmployee } from "../controllers/employees/deleteEmployee";
 import { editEmployee } from "../controllers/employees/editEmployee";
 import { getEmployees } from "../controllers/employees/getEmployees";
 import { roleAuth } from "../middlewares/roleAuth";
@@ -8,9 +9,8 @@ import { roleAuth } from "../middlewares/roleAuth";
 const EmployeesRouter = Router();
 
 EmployeesRouter.route("/").get(getEmployees).post(addEmployee);
-EmployeesRouter.route("/:id").post(
-	roleAuth(USER_ROLES.Manager!, USER_ROLES.Admin!),
-	editEmployee
-);
+EmployeesRouter.route("/:id")
+	.post(roleAuth(USER_ROLES.Manager!, USER_ROLES.Admin!), editEmployee)
+	.delete(roleAuth(USER_ROLES.Admin!), deleteEmployee);
 
 export default EmployeesRouter;
